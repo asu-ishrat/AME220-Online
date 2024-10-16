0d167bac772c6c9ea0917ed380f22d0b6388a021
ishrat <90342441+ishtotheizzo@users.noreply.github.com>
2024-10-16 11:09:02 -0700

    Update simpleJS.js
    Updated simpleJS.js

--- Week 5/simpleJS.js
+++ Week 5/simpleJS.js
@@ -1,5 +1,44 @@
-const person = {fname:"John", lname:"Doe", age:25};
-let text = "";
-for (let x in person) {
-  console.log(x)
+// lets define some variables 
+
+let numExample = 50
+var stringExample; 
+const pi = 3.14159
+
+//array Examples 
+
+let arrayExample = ["1", 1, "0", 0]
+
+//objectExample 
+const objectExample = { 
+    name: "ishrat", 
+    petName: "Siggy",
+    bothTogether: function() {
+        return this.name+this.petName
+    }
 }
+
+// math operators 
+
+let multiplicationExample = 50*2; 
+let divisionExample = 50/2; 
+let squaredExample = 10**2; 
+let moduloExample = 11%2; 
+
+console.log(`Multiplication Example: ${multiplicationExample} \n
+    Division Example:  ${divisionExample} \n
+    squaredExample: ${squaredExample}\n
+    Modulo Example: ${moduloExample}\n`); 
+
+// logical operators 
+
+numExample = 50
+
+console.log(numExample <=! 50)
+
+// basic add and or 
+
+// numExample must to be greater than 50 AND 60  (evaluates to false)
+console.log(numExample > 50 && numExample < 60)
+
+// numExample could to be greater than 50 OR 60  (evaluates to true)
+console.log(numExample > 50 || numExample < 60)
